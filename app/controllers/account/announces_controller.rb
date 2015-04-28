5d4c5338e1d7c14bdd3b205226081227933cb702
Mickael Giami <mickael.giami@gmail.com>
2015-04-28 19:00:34 +0200

    namespace

---
+++ app/controllers/account/announces_controller.rb
@@ -0,0 +1,58 @@
+module Account
+  class AnnouncesController < ApplicationController
+    before_action :find_announce, only: [:show, :edit, :update, :destroy ]
+
+
+    def index
+      @announces = current_user.announces
+    end
+
+    def new
+      @announce = current_user.announces.new
+    end
+
+    def create
+      @announce = current_user.announces.new(announce_params)
+      if @announce.save
+        redirect_to announce_path(@announce)
+      else
+        render :new
+      end
+    end
+
+    def show
+
+
+    end
+
+    def update
+      @announce.update(announce_params)
+
+      if @announce.save
+        redirect_to @announce
+      else
+        render :edit
+      end
+    end
+
+    def edit
+
+    end
+
+    def destroy
+      @announce.destroy
+      redirect_to root_path, notice: "l'annonce a bien été supprimée"
+
+    end
+
+    private
+
+    def announce_params
+      params.require(:announce).permit(:title, :price, :kind, :number_of_share, :reason, :company_id)
+    end
+
+    def find_announce
+      @announce = current_user.announces.find(params[:id])
+    end
+  end
+end
--- app/controllers/announces_controller.rb
+++ app/controllers/announces_controller.rb
@@ -1,56 +1,49 @@
 class AnnouncesController < ApplicationController
-  before_action :find_announce, only: [:show, :edit, :update, :destroy ]
-
-
+  skip_before_action :authenticate_user!
   def index
     @announces = Announce.all
   end
 
-  def new
-    @announce = Announce.new
+  # def new
+  #   @announce = Announce.new
 
-  end
+  # end
 
-  def create
-    @announce = Announce.new(announce_params)
-      if @announce.save
-        redirect_to announce_path(@announce)
-      else
-        render :new
-      end
-  end
+  # def create
+  #   @announce = Announce.new(announce_params)
+  #     if @announce.save
+  #       redirect_to announce_path(@announce)
+  #     else
+  #       render :new
+  #     end
+  # end
 
   def show
-
-
+    @announce = Announce.find(params[:id])
   end
 
-  def update
-    if @announce.update(announce_params)
-      redirect_to @announce
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   if @announce.update(announce_params)
+  #     redirect_to @announce
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-  def edit
+  # def edit
 
-  end
+  # end
 
-  def destroy
-    @announce.destroy
-      redirect_to root_path, notice: "l'annonce a bien été supprimée"
+  # def destroy
+  #   @announce.destroy
+  #     redirect_to root_path, notice: "l'annonce a bien été supprimée"
 
-  end
+  # end
 
   private
 
   def announce_params
     params.require(:announce).permit(:title, :price, :kind, :number_of_share, :reason, :company_id)
-  end
-
-  def find_announce
-    @announce = Announce.find(params[:id])
   end
 
 end
