157889dbc4b8e635de76fafb6e5eb5fec8936e25
Alexandre Cordani <alcordani@gmail.com>
2015-04-28 15:00:04 +0200

    facebook connect ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   resources :announces do
     resources :purchases, only: [:new, :create]
   end
   resources :companies, only: [:show, :index, :new]
 end
+
