9e5aaa0e84c53f0664c9b5570d2bc2ca32667bea
Alexandre Cordani <alcordani@gmail.com>
2015-04-29 15:13:53 +0200

    full

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   protect_from_forgery with: :exception
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, unless: :pages_controller?
 
   # after_action :verify_authorized, except:  :index, unless: :devise_or_pages_controller?
@@ -11,6 +11,10 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :address) }
+  end
 
   def devise_or_pages_controller?
     devise_controller? || pages_controller?
