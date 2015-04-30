3e38d470e71a825a11fa415a27805d90fce698ee
Mickael Giami <mickael.giami@gmail.com>
2015-04-30 14:05:51 +0200

    published default false

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429164001) do
+ActiveRecord::Schema.define(version: 20150430103457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,11 +36,11 @@ ActiveRecord::Schema.define(version: 20150429164001) do
     t.integer  "price"
     t.integer  "number_of_share"
     t.text     "reason"
-    t.boolean  "published"
+    t.boolean  "published",       default: false
     t.integer  "company_id"
     t.integer  "user_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.boolean  "cancelled"
     t.integer  "kind_id"
   end
