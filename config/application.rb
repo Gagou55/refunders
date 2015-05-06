863ea930fca46a794ed8d99f2348b81c5266517f
Mickael Giami <mickael.giami@gmail.com>
2015-05-07 00:43:50 +0200

    ok

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
-
 module Refunders
   class Application < Rails::Application
     config.i18n.enforce_available_locales = true
