c9bc8c8a16c343d20e363703f856b384c4ee60c9
Mickael Giami <mickael.giami@gmail.com>
2015-05-07 19:05:40 +0200

    erreur route

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,36 @@
 Rails.application.routes.draw do
-
-  get 'payments/new'
-
-  root to: 'pages#home'
-
   ActiveAdmin.routes(self)
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: 'registrations' }
 
-  namespace :account do
-    resources :announces
+  scope '(:locale)', locale: /fr|en/ do
+
+    root to: 'pages#home'
+    get 'payments/new'
+
+    namespace :account do
+      resources :announces
+    end
+
+    namespace :account do
+      resources :purchases
+    end
+
+    get '/cards', to: 'pages#cards'
+
+    resources :purchases, only: [:show, :create] do
+      resources :payments, only: [:new, :create]
+    end
+
+    resources :announces do
+      resources :messages,  only: [:create]
+      resources :purchases, only: [:new, :create]
+    end
+
+    resources :purchases, only: [:show, :index]
+
+    resources :companies, only: [:show, :index, :new, :create]
+
+    resources :users, only: [:show]
   end
-
-  namespace :account do
-    resources :purchases
-  end
-
-  get '/cards', to: 'pages#cards'
-
-resources :purchases, only: [:show, :create] do
-  resources :payments, only: [:new, :create]
-end
-
-  resources :announces do
-    resources :messages,  only: [:create]
-    resources :purchases, only: [:new, :create]
-  end
-
-  resources :purchases, only: [:show, :index]
-
-  resources :companies, only: [:show, :index, :new, :create]
-
-  resources :users, only: [:show]
-
 end
 
